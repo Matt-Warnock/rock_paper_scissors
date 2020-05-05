062161a2f95a0a339d11befdd706e62ec491785f
MattW <drflamerock@yahoo.co.uk>
2020-05-05 14:27:06 +0100

    Add display changes object

--- rps_game.js
+++ rps_game.js
@@ -14,8 +14,8 @@ function isDraw(userChoise, computerChoise) {
 
 function humanWins(userChoise, computerChoise) {
   return userChoise === 'rock' && computerChoise === 'scissors' ||
-  userChoise === 'paper' && computerChoise === 'rock' ||
-  userChoise === 'scissors' && computerChoise === 'paper';
+         userChoise === 'paper' && computerChoise === 'rock' ||
+         userChoise === 'scissors' && computerChoise === 'paper';
 }
 
 function resetComputerDisplay() {
@@ -65,3 +65,47 @@ document.getElementById('play').addEventListener('click', function () {
   playGame();
 
 });
+
+
+(function () {
+  const displayReaction = {
+    userMove: userGesture(),
+    computerMove: compGesture(),
+    _winSound: new Audio('audio/tada.mp3'),
+    _drawSound: new Audio('audio/kongas.mp3'),
+    _loseSound: new Audio('audio/Wrong-answer-sound-effect.mp3'),
+    _result: document.getElementById('result'),
+
+    resetComputerDisplay() {
+      document.getElementById('c_rock').classList.remove('selected');
+      document.getElementById('c_paper').classList.remove('selected');
+      document.getElementById('c_scissors').classList.remove('selected');
+    },
+
+    _updateDispay(printOnScreen, humanDisplay, robotDisplay) {
+      this._result.textContent = printOnScreen;
+      document.getElementById('human_face').src = humanDisplay;
+      document.getElementById('robot_face').src = robotDisplay;
+    },
+
+    playGame() {
+      document.getElementById(`c_${this.computerMove}`).classList.add('selected');
+
+      if (isDraw(userMove, computerMove)) {
+        this._drawSound.play();
+        return this._updateDispay('Its a draw!', 'images/human.png', 'images/robot.png');
+
+      } else if (humanWins(userMove, computerMove)) {
+        this._winSound.play();
+        return this._updateDispay('You win!', 'images/human-win.png', 'images/robot-sad.png');
+
+      } else {
+        this._loseSound.play();
+        return this._updateDispay('You lose!', 'images/human-sad.png', 'images/robot-win.png');
+      }
+    }
+  };
+
+
+
+})();
