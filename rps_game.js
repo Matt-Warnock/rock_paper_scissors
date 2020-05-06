630a7f4e43e7a87d08a05d0743f3fad026bb7083
MattW <drflamerock@yahoo.co.uk>
2020-05-06 12:09:49 +0100

    Refactor suggested changes

--- rps_game.js
+++ rps_game.js
@@ -1,72 +1,3 @@
-/* function userGesture() {
-  return document.querySelector('input[name=gesture]:checked').value;
-}
-
-function compGesture() {
-  var gestureChoice = ['rock', 'paper', 'scissors'];
-  var randomIndex = Math.floor(Math.random() * 3);
-  return gestureChoice[randomIndex];
-}
-
-function isDraw(userChoise, computerChoise) {
-  return userChoise === computerChoise;
-}
-
-function humanWins(userChoise, computerChoise) {
-  return userChoise === 'rock' && computerChoise === 'scissors' ||
-         userChoise === 'paper' && computerChoise === 'rock' ||
-         userChoise === 'scissors' && computerChoise === 'paper';
-};
-
-function resetComputerDisplay() {
-  document.getElementById('c_rock').classList.remove('selected');
-  document.getElementById('c_paper').classList.remove('selected');
-  document.getElementById('c_scissors').classList.remove('selected');
-}
-
-function updateDispay(printOnScreen, humanDisplay, robotDisplay) {
-  result.textContent = printOnScreen;
-  document.getElementById('human_face').src = humanDisplay;
-  document.getElementById('robot_face').src = robotDisplay;
-}
-
-function playGame() {
-  let userMove = userGesture();
-  let computerMove = compGesture();
-
-  const winSound = new Audio('audio/tada.mp3');
-  const drawSound = new Audio('audio/kongas.mp3');
-  const loseSound = new Audio('audio/Wrong-answer-sound-effect.mp3');
-
-  const result = document.getElementById('result');
-
-
-  document.getElementById(`c_${computerMove}`).classList.add('selected');
-
-
-  if (isDraw(userMove, computerMove)) {
-    drawSound.play();
-    return updateDispay('Its a draw!', 'images/human.png', 'images/robot.png');
-
-  } else if (humanWins(userMove, computerMove)) {
-    winSound.play();
-    return updateDispay('You win!', 'images/human-win.png', 'images/robot-sad.png');
-
-  } else {
-    loseSound.play();
-    return updateDispay('You lose!', 'images/human-sad.png', 'images/robot-win.png');
-  }
-}
-
-
-document.getElementById('play').addEventListener('click', function () {
-
-  resetComputerDisplay();
-  playGame();
-
-}); */
-
-
 (function () {
   let userInput = {
     userGesture () {
@@ -74,8 +5,8 @@ document.getElementById('play').addEventListener('click', function () {
     },
     clickToPlay () {
       document.getElementById('play').addEventListener('click', function () {
-        displayReaction.resetComputerDisplay();
-        displayReaction.playGame();
+        gameLayout.resetComputerDisplay();
+        gameLayout.playGame();
       });
     }
   };
@@ -84,7 +15,7 @@ document.getElementById('play').addEventListener('click', function () {
     _gestureChoice: ['rock', 'paper', 'scissors'],
 
     get compGesture() {
-      randomIndex = Math.floor(Math.random() * 3);
+      let randomIndex = Math.floor(Math.random() * this._gestureChoice.length);
       return this._gestureChoice[randomIndex];
     },
 
@@ -99,7 +30,7 @@ document.getElementById('play').addEventListener('click', function () {
     }
   };
 
-  const displayReaction = {
+  const gameLayout = {
     _winSound: new Audio('audio/tada.mp3'),
     _drawSound: new Audio('audio/kongas.mp3'),
     _loseSound: new Audio('audio/Wrong-answer-sound-effect.mp3'),
